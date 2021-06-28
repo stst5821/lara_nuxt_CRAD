a215f0cead1f92c71700662307942e46b80f5b7a
stst5821 <stst5821@gmail.com>
2021-06-28 18:32:38 +0900

    test

--- config/database.php
+++ config/database.php
@@ -78,12 +78,13 @@ return [
         //     'sslmode' => 'prefer',
         // ],
 
+        // Herokuデプロイ用
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'ec2-54-91-188-254.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel_development'),
+            'database' => env('DB_DATABASE', 'laravel_develop'),
             'username' => env('DB_USERNAME', 'ixfrspqoeaflfr'),
             'password' => env('DB_PASSWORD', '2e3115bcbb55149e1594c0e7e2e1c912f852c6c3719c517ba7e8332b79aef3f2'),
             'charset' => 'utf8',
